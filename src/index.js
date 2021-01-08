8fb73ffd3a066c5c9127bbc5c14d93b1464f2af9
vayipalayamchand <vayipalayamchand@paypal.com>
2021-01-08 10:42:42 -0600

    Added logic to calculate winner
    * Checked all patterns to calculate winner
    * Currently hardcoded for 3x3 board
    * Ensured no squares can be clicked after winner is declared.

--- src/index.js
+++ src/index.js
@@ -23,8 +23,32 @@ function Square(props) {
       }
     }
 
+    calculateWinner(squares){
+      const winningPatterns = [
+          [0,1,2],
+          [3,4,5],
+          [6,7,8],
+          [0,3,6],
+          [1,4,7],
+          [2,5,8],
+          [0,4,8],
+          [2,4,6]
+      ];
+      for(let i=0; i< winningPatterns.length; i++){
+        const [a,b,c] = winningPatterns[i];
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+          return squares[a];
+        }
+      }
+      return null;
+    }
+
     handleClick(i){
       const squares = this.state.squares.slice();
+      // Return when there is a winner or the square is already filled
+      if(this.calculateWinner(squares) || squares[i]){
+        return;
+      }
       squares[i] = this.state.xIsNext? 'X':'O';
       this.setState({
         squares: squares,
@@ -38,8 +62,13 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: X';
-  
+      const winner = this.calculateWinner(this.state.squares);
+      let status;
+      if(winner){
+        status = 'Winner is :' + winner;
+      } else {
+        status = this.state.xIsNext ? 'Next player: X' : 'Next player: O';
+      }
       return (
         <div>
           <div className="status">{status}</div>
